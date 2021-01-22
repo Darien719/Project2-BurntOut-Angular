1ece8ccf45eb20e3833afc7d1e98ed635c8ec6e3
John <jpj46@nau.edu>
2021-01-22 13:26:30 -0700

    Layout for validation for user sign up

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, NgModel, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { SignupService } from '../services/signup.service';
 import { User } from '../services/user';
@@ -23,27 +23,9 @@ export class SignUpComponent implements OnInit {
                     'email':'', 'userRole':'', 'company':'',  
                     'username':'', 'password':'', 'salt':''};
 
-  signUpForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    email: new FormControl(''),
-    userRole: new FormControl(''),
-    company: new FormControl(''),
-    username: new FormControl(''),
-    password: new FormControl('')
-  })
-
   constructor(private router: Router, private signUpServ: SignupService) { }
 
-  signUpPost(signUp: FormGroup){
-    this.newUser.firstName = signUp.get('firstName').value;
-    this.newUser.lastName = signUp.get('lastName').value;
-    this.newUser.email = signUp.get('email').value;
-    this.newUser.company = signUp.get('company').value;
-    this.newUser.userRole = signUp.get('userRole').value;
-    this.newUser.username = signUp.get('username').value;
-    this.newUser.password = signUp.get('password').value;
-
+  signUpPost(){
     console.log(this.newUser.firstName);
     console.log(this.newUser.lastName);
     console.log(this.newUser.email);
