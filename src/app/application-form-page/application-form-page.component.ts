4f0a2c017ef38060da48ca18067be42d61ee8791
Olga Melnikova <olgamelnikoff@gmail.com>
2021-01-23 18:03:09 -0500

    Implemented Upload Resume functionality with S3 bucket

--- src/app/application-form-page/application-form-page.component.ts
+++ src/app/application-form-page/application-form-page.component.ts
@@ -13,10 +13,10 @@ import {FileUploadService} from '../services/file-upload.service';
 export class ApplicationFormPageComponent implements OnInit {
   pageTitle = "Application Form";
   toFile;
- 
-    //private fileUploadServ: FileUploadService;
 
-  constructor(private createAppServ: CreateApplicationService, private sessServ:SessionService) { }
+  constructor(private createAppServ: CreateApplicationService, private sessServ:SessionService
+    
+    , private fileUploadServ: FileUploadService) { }
 
   username:string;
   posting_id:number;
@@ -32,7 +32,6 @@ export class ApplicationFormPageComponent implements OnInit {
   applicationgroup = new FormGroup({
     posting_id: new FormControl(''),
     username: new FormControl(''),
-    //resume: new FormControl(''),
   })
 
   ngOnInit(): void {
@@ -44,19 +43,11 @@ export class ApplicationFormPageComponent implements OnInit {
     throw new Error('Method not implemented.');
   }
 
-/*   jobPost(jobgroup :FormGroup){
-
-    this.application.posting_id = this.applicationgroup.get('posting_id').value;
-    this.application.username = this.applicationgroup.get('username').value;
-  } */
-
-  /* handleFileInput(files: FileList) {
-    this.resume = files.item(0);
-  } */
-
   submitApplication(apgroup : FormGroup) : void {
     this.application.posting_id = this.applicationgroup.get('posting_id').value;
     this.application.username = this.applicationgroup.get('username').value;
+    this.application.resume = this.resume;
+    this.submit();
     this.createAppServ.postApplication(this.application).subscribe(
       response=>{
         console.log("In response");
@@ -68,16 +59,13 @@ export class ApplicationFormPageComponent implements OnInit {
     )
   }
 
-/*   submit() {
+  submit() {
     const file = this.toFile.item(0);
-    this.fileUploadServ.fileUpload(file);
+    this.fileUploadServ.uploadFile(file, this.username);
   }
 
   onChange(event) {
     this.toFile = event.target.files;
-
-  } */
-
-  
-  
+    this.resume = this.toFile;
+  }
 }
--- src/app/services/file-upload.service.ts
+++ src/app/services/file-upload.service.ts
@@ -1,55 +1,43 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-/* import * as AWS from 'aws-sdk/global';
-import * as S3 from 'aws-sdk/clients/s3'; */
+import * as S3 from 'aws-sdk/clients/s3';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FileUploadService {
+  
 
   constructor(private httpClient: HttpClient) { }
 
-/*   fileUpload(file) {
+  handleError(e: any) {
+    throw new Error('Method not implemented.');
+  }
+
+  uploadFile(file, username) {
     const contentType = file.type;
     const bucket = new S3(
           {
               accessKeyId: 'AKIAZTURDFD45TH2H4MC',
               secretAccessKey: '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',
-              region: 'ap-south-1',
-            
+              region: 'us-east-1'
           }
       );
       const params = {
           Bucket: 'burntout',
-          Key:  file.name,
+          Key: file.name,
           Body: file,
           ACL: 'public-read',
           ContentType: contentType
       };
       bucket.upload(params, function (err, data) {
           if (err) {
-              console.log('EROOR: ',JSON.stringify( err));
+              console.log('There was an error uploading your file: ', err);
               return false;
           }
-          console.log('File Uploaded.', data);
+          console.log('Successfully uploaded file.', data);
           return true;
       });
-    } */
-
-  
-
-  /* postFile(fileToUpload: File): Observable<boolean> {
-    const endpoint = 'http://localhost:9025/application';
-    const formData: FormData = new FormData();
-    formData.append('fileKey', fileToUpload, fileToUpload.name);
-    return this.httpClient
-      .post(endpoint, formData, { headers: yourHeadersConfig })
-      .map(() => { return true; })
-      .catch((e) => this.handleError(e));
-} */
-  handleError(e: any) {
-    throw new Error('Method not implemented.');
-  }
+}
 }
