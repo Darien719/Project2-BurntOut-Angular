4af4b0699848dbda9965f1405839d81357d6df0f
Darien719 <Dsosa029@fiu.edu>
2021-01-28 12:48:55 -0500

    Testing of testing

--- src/app/view-self-jobpostings/view-self-jobpostings.component.spec.ts
+++ src/app/view-self-jobpostings/view-self-jobpostings.component.spec.ts
@@ -1,10 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { JobService } from '../services/job.service';
 
 import { ViewSelfJobpostingsComponent } from './view-self-jobpostings.component';
 
 describe('ViewSelfJobpostingsComponent', () => {
   let component: ViewSelfJobpostingsComponent;
   let fixture: ComponentFixture<ViewSelfJobpostingsComponent>;
+  let router: Router;
+  let jobServ: JobService;
+  let mockClient: {get: jasmine.Spy, post: jasmine.Spy, put:jasmine.Spy, delete: jasmine.Spy}
+  
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -23,3 +29,4 @@ describe('ViewSelfJobpostingsComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
